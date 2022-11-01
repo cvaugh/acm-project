99b659c50f4c0c72a0813ed6a7aa12a806c09681
Chase Vaughan <git@cvaugh.dev>
2022-10-31 22:59:40 -0400

    Add boilerplate HTML/CSS/PHP

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+include_once "__serverconfig.php";
+
+# -------------------------------------
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+# -------------------------------------
+
+?>
